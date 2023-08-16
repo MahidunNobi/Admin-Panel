f0420c3d97820c82ae6ec7a73620e7a094c5e1b9
MahidunNobi <mdmahidunnobi@gmail.com>
2023-08-16 08:52:56 +0600

    1d2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home/Home"
+import List from "./pages/List/List"
+import Login from "./pages/Login/Login"
+import New from "./pages/New/New"
+import Single from "./pages/Single/Single"
+import Navbar from "./componants/Common-Componants/Navbar/Navbar"
+import Sidebar from "./componants/Common-Componants/Sidebar/Sidebar"
 
 function App() {
 
 
   return (
     <>
-      <h1 className='text-4xl text-red-600'> Hello</h1>
+    <Narbar />
+    <Routes>
+      <Route path='/'>
+        <Route index element={<Home />} /> 
+        <Route path='login' element={<Login />} />
+        <Route path='user'>
+          <Route index element={<List />} />
+          <Route path=':id' element={<Single/>} />
+          <Route path='new' element={<New/>} />
+        </Route>
+        <Route path='products'>
+          <Route index element={<List />} />
+          <Route path=':id' element={<Single/>} />
+          <Route path='new' element={<New/>} />
+        </Route>
+      </Route>
+    </Routes>      
     </>
   )
 }
